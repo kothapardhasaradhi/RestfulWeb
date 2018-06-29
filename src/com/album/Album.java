9d94cd822f034c5fcb220fd092c5116e95753607
Pardha Saradhi Kotha (HEALTH) <PA391763@wipro.com>
2018-06-29 18:16:23 +0530

    friday commit

---
+++ src/com/album/Album.java
@@ -0,0 +1,20 @@
+package com.album;
+
+public class Album {
+	
+	public String title;
+	public String singer;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSinger() {
+		return singer;
+	}
+	public void setSinger(String singer) {
+		this.singer = singer;
+	}
+
+}
